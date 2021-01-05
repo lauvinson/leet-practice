6abdf7051ddcb7707b66d98a8f60b6e37a4b499b
vinson <liuws@zhiyun-tech.com>
2021-01-05 14:41:43 +0800

    first commit

---
+++ top-interview/只出现一次的数字.py
@@ -0,0 +1,43 @@
+"""
+从指定集合中找出只出现一次的数字
+方案：
+1.遍历集合，利用额外空间存储出现次数或筛选重复
+2.利用去重集合加法运算和原集合加法运算之差找出元素 *
+2.使用亦或找出元素，自身亦或为0且满足交换律和结合律，例如亦或可实现无中间变量交换整型变量
+
+总结：方案大致区别在于空间复杂度O(1)和O(n)，引入额外存储空间也带来了一些操作复杂性
+"""
+
+input1 = [2, 2, 1]
+input2 = [4, 1, 2, 1, 2]
+
+
+def a():
+    def find_single(i):
+        r = 0
+        for v in i:
+            r = r ^ v
+        return r
+
+    print(find_single(input1))
+    print(find_single(input2))
+
+
+def b():
+    def find_single(i):
+        m = {}
+        for v in i:
+            if v in m:
+                m[v] = m[v] + 1
+            else:
+                m[v] = 0
+        for v in m:
+            if m[v] == 0:
+                return v
+
+    print(find_single(input1))
+    print(find_single(input2))
+
+
+a()
+b()
